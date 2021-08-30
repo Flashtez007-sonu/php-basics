25112c35801960cab9ad52cb3c839032be6537ee
Shubham Gupta <68388052+Flashtez007-sonu@users.noreply.github.com>
2021-08-30 09:38:32 +0530

    sum of digits

--- sum_of_digits.php
+++ sum_of_digits.php
@@ -1,3 +1,12 @@
+//To find sum of digits of a number just add all the digits.
+
+Logic:
+
+Take the number.
+Divide the number by 10.
+Add the remainder to a variable.
+Repeat the process until remainder is 0.
+
 <?php 
     $num = 145239;
     $sum = 0; $rem = 0;
@@ -10,3 +19,6 @@
     echo "sum of digits 145239 is $sum";
 
 ?>
+
+OUTPUT:
+sum of digits 145239 is 24
