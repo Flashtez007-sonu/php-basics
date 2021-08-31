4105014d13a3cbeb6576dc2c51b4bab537e97385
Shubham Gupta <68388052+Flashtez007-sonu@users.noreply.github.com>
2021-08-31 12:52:58 +0530

    Reversing Number With strrev() in PHP

---
+++ Reverse_number_strrev.php
@@ -0,0 +1,10 @@
+<?php
+    function reverse($number)
+    {
+        $num = (string)$number;
+        $revstr = strrev($num);
+        $reverse = (int)$revstr;
+        return $reverse;
+    }
+    echo reverse(23456);
+?>
